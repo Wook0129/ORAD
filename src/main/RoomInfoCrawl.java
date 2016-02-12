4fe56beadc809b87223ed86e45ba341d5ea3a541
Taewook Kim <taewook@dm.snu.ac.kr>
2016-02-13 03:00:04 +0900

    First Upload

--- src/main/RoomInfoCrawl.java
+++ src/main/RoomInfoCrawl.java
@@ -23,10 +23,10 @@ public class RoomInfoCrawl {
 	static ArrayList<String> data = new ArrayList<String>();
 
 	static HashMap<String, Integer> textFeatures = new HashMap<String, Integer>(); //Whole term dictionary
-	static ArrayList<HashMap<String, Integer>> termFreqs = new ArrayList<HashMap<String, Integer>>(); //Index -> (Term -> Freq)
+	static ArrayList<HashSet<String>> termFreqs = new ArrayList<HashSet<String>>(); //Index -> (Term Set)
 	static KeywordExtractor ke;
-	static boolean useTextFeatures = true;
-	static int termFreqCutoff = 10;
+	static boolean useTextFeatures = false;
+	static int termFreqCutoff = 30;
 
 	public static void main(String[] args) throws Exception{
 
@@ -35,8 +35,8 @@ public class RoomInfoCrawl {
 		String seorim = "lat=37.47504425048828&lng=126.93497467041016";
 		String inhun = "lat=37.475128173828125&lng=126.96526336669922";
 
-		String[] locations = {naksung, daehak, seorim, inhun};
-		//		String[] locations = {naksung};
+		//		String[] locations = {naksung, daehak, seorim, inhun};
+		String[] locations = {naksung};
 
 		HashSet<String> links = new HashSet<String>();
 
@@ -47,14 +47,14 @@ public class RoomInfoCrawl {
 		driver.quit();
 
 		FileWriter fw = new FileWriter("C://Users/태욱/Desktop/원룸정보.csv");
-		String vars = "보증금;방 구조;월세;건물형태;등록번호;층/건물층수;관리비;주차;관리비 포함항목;엘레베이터;크기(평);옵션;입주가능일;주소;인근전철역;등록자 정보;비고";
-		
+		String vars = "보증금;방 구조;월세;건물형태;등록번호;층/건물층수;관리비;주차;관리비_가스포함;관리비_전기포함;관리비_수도포함;관리비_인터넷포함;관리비_TV포함;엘레베이터;크기(평);옵션;입주가능일;주소;인근전철역;등록자 정보;비고";
+
 		int cnt = 0;
 
 		if(useTextFeatures) ke = new KeywordExtractor();
 
 		for(String link : links) {
-			data.add(getRoomInfo(link, useTextFeatures));
+			data.add(getRoomInfo(link));
 			cnt++;
 			if(cnt % 10 == 0) System.out.println(cnt+"번째 원룸 정보 처리 중..");
 		}
@@ -65,19 +65,19 @@ public class RoomInfoCrawl {
 			for(String textFeature : textFeatures.keySet())
 				if(textFeatures.get(textFeature) > termFreqCutoff) dic.add(textFeature);
 			for(String textFeature : dic)
-				vars += ";"+textFeature;
+				vars += ";"+"term:"+textFeature;
 
 			for(int i = 0; i<data.size(); i++){
 				String room_data = data.get(i);
-				HashMap<String, Integer> termFreq = termFreqs.get(i);
+				HashSet<String> termFreq = termFreqs.get(i);
 				for(String term : dic)
-					if(termFreq.containsKey(term)) room_data += termFreq.get(term)+";";
+					if(termFreq.contains(term)) room_data += 1+";";
 					else room_data += 0+";";
 				room_data = room_data.substring(0, room_data.length() - 1);
 				data.set(i,room_data);
 			}
 		}
-		
+
 		fw.write(vars+"\n");
 		for(String roomData : data) {
 			fw.write(roomData + "\n");
@@ -104,7 +104,7 @@ public class RoomInfoCrawl {
 		}
 	}
 
-	public static String getRoomInfo(String link, boolean useTextFeatures) throws IOException{
+	public static String getRoomInfo(String link) throws IOException{
 		String url = "https://www.zigbang.com"+link;
 		Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(0).get();
 		String raw_roomInfo = doc.getElementsByClass("detail-list").first().toString();
@@ -129,6 +129,16 @@ public class RoomInfoCrawl {
 				case "월세":
 					value = value.replaceAll("만원", "").trim();
 					break;
+				case "층/건물층수":
+					String temp_floor = value.split("\\/")[0];
+					if(temp_floor.contains("반지하")) value = "-1";
+					else if(temp_floor.contains("고층")||temp_floor.contains("중층")||temp_floor.contains("저층")) value = "아파트";
+					else{
+						int floor = Integer.parseInt(temp_floor.replaceAll("층", ""));
+						if(floor == 1 || floor == 2) value = String.valueOf(floor);
+						else value = "3+";
+					}
+					break;
 				case "크기":
 					value = value.split("\\(")[1].split("P")[0];
 					break;
@@ -139,6 +149,20 @@ public class RoomInfoCrawl {
 				case "주차":
 					if(value.equals("불가능")) value = "N";
 					else value = "Y";
+					break;
+				case "관리비 포함항목":
+					String temp = "";
+					if(value.contains("가스")) temp += "Y;";
+					else temp += "N;";
+					if(value.contains("전기")) temp += "Y;";
+					else temp += "N;";
+					if(value.contains("수도")) temp += "Y;";
+					else temp += "N;";
+					if(value.contains("인터넷")) temp += "Y;";
+					else temp += "N;";
+					if(value.contains("TV")) temp += "Y";
+					else temp += "N";
+					value = temp;
 					break;
 				case "엘리베이터":
 					if(value.equals("없음")) value = "N";
@@ -164,14 +188,14 @@ public class RoomInfoCrawl {
 
 						//Keyword Extraction(Kokoma)
 						KeywordList kl = ke.extractKeyword(value, true);
-						HashMap<String, Integer> termFreq = new HashMap<String, Integer>();
+						HashSet<String> termFreq = new HashSet<String>();
 						for( int i = 0; i < kl.size(); i++ ) {
 							Keyword kwrd = kl.get(i);
 							String keyword = kwrd.getString();
 							if(keyword.length() == 2 || keyword.length() == 3){
 								if(textFeatures.containsKey(keyword)) textFeatures.put(keyword, textFeatures.get(keyword) + kwrd.getCnt());
 								else textFeatures.put(keyword, kwrd.getCnt());
-								termFreq.put(keyword, kwrd.getCnt());
+								termFreq.add(keyword);
 							}
 						}
 						termFreqs.add(termFreq);
